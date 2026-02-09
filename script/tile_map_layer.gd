2e57ef92aff74d4662c1c09451cbeeafadb5dd84
Virik12 <kubaa0022@gmail.com>
2026-02-09 19:28:49 +0100

    chujowa naprawa

--- script/tile_map_layer.gd
+++ script/tile_map_layer.gd
@@ -24,6 +24,19 @@ func check_surroundings_atlas(cells):
 			how_many_black += 1
 	return how_many_black
 
+func check_need_change (Vector:Vector2,how_many_black:int) -> bool:
+	var temp = get_cell_atlas_coords(Vector)
+	if temp.y == 0:
+		if how_many_black < 2 or how_many_black >3:
+			return true
+		else:
+			return false
+	else:
+		if how_many_black == 3:
+			return true
+		else:
+			return false
+
 func set_cell_blackV2(Vector:Vector2):
 	set_cell(Vector,1,Vector2i(0,0))
 
@@ -60,12 +73,16 @@ func bad_simulation():
 
 func good_simulation():
 	var store_data:Array =[]
+	var store_data_size:int = 0
 	for i in range(1,GAMESIZEX + 1):
 		for j in range(1,GAMESIZEY + 1):
 			var surroundings_cords=get_surroundings(Vector2i(i-GAMESIZEX/2,j-GAMESIZEY/2))
 			var how_many_black=check_surroundings_atlas(surroundings_cords)
-			store_data.push_front(Vector3(i-GAMESIZEX/2,j-GAMESIZEY/2,how_many_black))
-	for i in range(GAMESIZEX*GAMESIZEY):
+			var check_need_change=check_need_change(Vector2i(i-GAMESIZEX/2,j-GAMESIZEY/2),how_many_black)
+			if check_need_change == true:
+				store_data.push_front(Vector3(i-GAMESIZEX/2,j-GAMESIZEY/2,how_many_black))
+				store_data_size +=1
+	for i in range(store_data_size):
 		set_cell_V3(store_data[i])
 
 func do_simulation():
@@ -79,10 +96,9 @@ func _ready() -> void:
 	Engine.time_scale = GAMESPEED
 	
 
-func _process(delta: float) -> void:
-	#do_simulation()
-	pass
 
+func _process(delta: float) -> void:
+	pass
 
 
 func _input(event: InputEvent) -> void:
